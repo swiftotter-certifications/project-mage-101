20bc95f0990573181c2fc991d9c9bbd09d5be895
Chris Nanninga <tarsusc81@gmail.com>
2023-02-01 20:36:20 -0600

    Update view model to use store config value

--- src/example-product-questions/ViewModel/ProductView/Questions.php
+++ src/example-product-questions/ViewModel/ProductView/Questions.php
@@ -7,12 +7,21 @@ declare(strict_types=1);
 
 namespace SwiftOtter\ProductQuestions\ViewModel\ProductView;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class Questions implements ArgumentInterface
 {
+    private ScopeConfigInterface $scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function getHeading(): string
     {
-        return 'Hello world from a new Product Questions module!';
+        return 'Hello world from ' . $this->scopeConfig->getValue('web/secure/base_url');
     }
 }
