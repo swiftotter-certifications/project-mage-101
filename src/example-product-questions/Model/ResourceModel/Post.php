d573098e8b0ea459994d82bc091859059ec5daab
Chris Nanninga <tarsusc81@gmail.com>
2023-02-20 11:26:41 -0600

    Full page cache support

--- src/example-product-questions/Model/ResourceModel/Post.php
+++ src/example-product-questions/Model/ResourceModel/Post.php
@@ -7,15 +7,46 @@ declare(strict_types=1);
 
 namespace SwiftOtter\ProductQuestions\Model\ResourceModel;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Framework\Model\ResourceModel\Db\Context;
 
 class Post extends AbstractDb
 {
+    private ProductRepositoryInterface $productRepository;
+
+    public function __construct(
+        Context $context,
+        ProductRepositoryInterface $productRepository,
+        $connectionName = null
+    ) {
+        parent::__construct($context, $connectionName);
+        $this->productRepository = $productRepository;
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function _construct()
     {
         $this->_init('productquestions_post', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _beforeSave(\Magento\Framework\Model\AbstractModel $object)
+    {
+        if (!$object->hasProduct() && ($productId = $object->hasProductId())) {
+            try {
+                $product = $this->productRepository->getById($productId);
+            } catch (\Exception $e) {
+                $product = null;
+            }
+
+            $object->setProduct($product);
+        }
+
+        return parent::_beforeSave($object);
     }
 }
