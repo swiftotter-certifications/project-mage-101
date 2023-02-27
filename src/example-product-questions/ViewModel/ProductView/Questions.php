a5761822ac2b67961776270f0b6bcce9bedf1403
Chris Nanninga <tarsusc81@gmail.com>
2023-02-27 17:22:55 -0600

    Display question and answer content on PDP

--- src/example-product-questions/ViewModel/ProductView/Questions.php
+++ src/example-product-questions/ViewModel/ProductView/Questions.php
@@ -7,21 +7,58 @@ declare(strict_types=1);
 
 namespace SwiftOtter\ProductQuestions\ViewModel\ProductView;
 
+use Magento\Catalog\Model\Product;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use SwiftOtter\ProductQuestions\Model\Config;
+use SwiftOtter\ProductQuestions\Model\ResourceModel\Post\Collection as PostCollection;
+use SwiftOtter\ProductQuestions\Model\ResourceModel\Post\CollectionFactory as PostCollectionFactory;
+use Magento\Framework\Registry;
 
 class Questions implements ArgumentInterface
 {
+    private ?Product $product = null;
+
     private Config $config;
+    private PostCollectionFactory $postCollectionFactory;
+    private Registry $coreRegistry;
 
     public function __construct(
-        Config $config
+        Config $config,
+        PostCollectionFactory $postCollectionFactory,
+        Registry $coreRegistry
     ) {
         $this->config = $config;
+        $this->postCollectionFactory = $postCollectionFactory;
+        $this->coreRegistry = $coreRegistry;
     }
 
     public function getHeading(): string
     {
         return $this->config->getHeading();
+    }
+
+    public function getQuestions(): array
+    {
+        $product = $this->getProduct();
+        if (!$product) {
+            return [];
+        }
+
+        /** @var PostCollection $posts */
+        $posts = $this->postCollectionFactory->create();
+        $posts->addProductIdFilter((int) $product->getId())
+            ->addQuestionsOnlyFilter()
+            ->addAnswers()
+            ->addOrder('created_at', 'DESC');
+
+        return $posts->getItems();
+    }
+
+    private function getProduct(): ?Product
+    {
+        if ($this->product === null) {
+            $this->product = $this->coreRegistry->registry('product');
+        }
+        return $this->product;
     }
 }
