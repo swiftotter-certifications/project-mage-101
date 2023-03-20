e8ebc0b8b7c677add12065d9346f6deebcfc256e
Chris Nanninga <tarsusc81@gmail.com>
2023-03-20 15:47:31 -0500

    Bootstrap theme

---
+++ src/example-theme-sandbox/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @by SwiftOtter, Inc.
+ * @website https://swiftotter.com
+ **/
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/SwiftOtter/sandbox', __DIR__);
